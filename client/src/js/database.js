877aab53d696f9717497f586849519dd4eb4b250
dspnc <dspence.hill@gmail.com>
2023-06-08 13:19:32 -0400

    updated database.js to get and update the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const id = await store.add(content);
+  console.log(`Content ${content} added to database with id ${id}`)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const transaction = db.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const results = await store.getAllKeys();
+  const content = [];
+  for (const id of results) {
+    const value = await store.get(id);
+    content.push(value);
+  }
+  return content;
+}
 
 initdb();
